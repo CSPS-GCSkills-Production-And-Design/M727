6609c695e51fafa651e7ff7d8fe8d9bb281026b4
csabourin <christian.sabourin@canada.ca>
2020-05-06 15:09:34 -0400

    First commit

---
+++ Dev/settings/settings-general.js
@@ -0,0 +1,27 @@
+'use strict';
+
+define({
+    "courseLegacyCode":  "M726",
+	"courseTitle_en": 	 "Operations, Maintenance and Commissioning",
+	"courseTitle_fr": 	 "Exploitation, entretien et mise en service",
+	"courseSubtitle_en": "",
+	"courseSubtitle_fr": "",
+	"seriesTitle_en": 	 "<span class='first'>Operations, Maintenance</span><span class='second'>and Commissioning</span>",
+	"seriesTitle_fr": 	 "<span class='first'>Exploitation, entretien</span><span class='second'> et mise en service</span>",
+
+	"showSitemap":	  false,
+	"showToolbox": 	  true,
+	
+	"requireLoadPageScript": true,
+	
+	/*addToolboxPage:[		
+		{ "name_en":"Documents", "name_fr":"Documents", "filename_en":"documents_en", "filename_fr":"documents_fr"}	
+	],*/
+		
+	"triggerCompletionWhenAllPagesViewed": true,
+	"markModuleAsViewedOnLastPage": false,
+	
+	"enableMicroLearning": false,
+	"enableTutorial": false
+});
+ 
